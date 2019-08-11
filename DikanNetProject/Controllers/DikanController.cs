a9bf80ffe7aec83babe971d16d947e8bbf651411
Roi Israeli <roi467@gmail.com>
2019-08-11 16:08:15 +0300

    Dikan Ui

---
+++ DikanNetProject/Controllers/DikanController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DikanNetProject.Controllers
+{
+    public class DikanController : Controller
+    {
+        // GET: Dikan
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
