7dc559bbb5c1c7ea881320c0fd4c3083d4fb6a3b
Ron Nahmias <ron.nahmias2@gmail.com>
2019-08-21 00:47:28 +0300

    Dikan - sp and volunteer places

--- DataEntities/SpDefinition.cs
+++ DataEntities/SpDefinition.cs
@@ -13,14 +13,25 @@ namespace DataEntities
     {
         [Key]
         public int ScholarshipID { get; set; }
+
+        [Display(Name ="שם מלגה")]
         public string ScholarshipName { get; set; }
+
+        [Display(Name = "סכום מלגה")]
         public int ScholarshipAmount { get; set; }
+
+        [Display(Name = "תיאור מלגה")]
         public string ScholarshipDetails { get; set; }
 
-        [Column("Typee")]
+        [Column("Typee"),Display(Name = "סוג מלגה")]
         public string Type { get; set; }
+
+        [Display(Name = "תאריך תחילת הגשה")]
         public DateTime DateOpenScholarship { get; set; }
+
+        [Display(Name = "תאריך אחרון להגשה")]
         public DateTime DateDeadLine { get; set; }
+
         public virtual ICollection<SpSocio> Socioeconomicc { get; set; }
         public virtual ICollection<SpHalacha> InPractices { get; set; }
         public virtual ICollection<SpExcellence> ExcellenceStudents { get; set; }
--- DikanNetProject/Controllers/DikanController.cs
+++ DikanNetProject/Controllers/DikanController.cs
@@ -16,27 +16,139 @@ namespace DikanNetProject.Controllers
             return View();
         }
 
-        #region Create ScholarShip Definition
+        #region ScholarShip Section
 
         [HttpGet]
-        public ActionResult CreateSp()
+        public ActionResult CreateEditSp(int? id)
         {
-            return View();
+            ViewBag.Header = "עדכון מלגה";
+            SpDefinition TempSp;
+            using (DikanDbContext ctx = new DikanDbContext())
+            {
+                TempSp = ctx.SpDef.Where(s => s.ScholarshipID == id).FirstOrDefault();
+            }
+            if (TempSp != null)
+                return View(TempSp);
+            else
+            {
+                ViewBag.Header = "הוספת מלגה";
+                return View();
+            }
+                
         }
 
         [HttpPost]
-        public ActionResult CreateSp(SpDefinition NewSp)
+        public ActionResult CreateEditSp(SpDefinition ClientSp)
         {
+            string res = string.Empty;
             if(ModelState.IsValid)
             {
                 using(DikanDbContext ctx = new DikanDbContext())
                 {
-                    ctx.SpDef.Add(NewSp);
+                    SpDefinition Temp = ctx.SpDef.Where(s => s.ScholarshipID == ClientSp.ScholarshipID).FirstOrDefault();
+                    if (Temp == null)
+                    {
+                        res = "מלגה נוספה בהצלחה";
+                        ctx.SpDef.Add(ClientSp);
+                    }
+                    else
+                    {
+                        res = "מלגה עודכנה בהצלחה";
+                        ctx.Entry(Temp).CurrentValues.SetValues(ClientSp);
+                    }
                     ctx.SaveChanges();
                 }
             }
-            return View();
+            return RedirectToAction("SpList",new { response = res });
         }
+
+        [HttpGet]
+        public ActionResult SpList(string response = "")
+        {
+            ViewBag.response = response;
+            List<SpDefinition> SpList;
+            using (DikanDbContext ctx = new DikanDbContext())
+            {
+                SpList = ctx.SpDef.ToList();
+            }
+            return View(SpList);
+        }
+        #endregion
+
+        #region Volunter Places
+        [HttpGet]
+        public ActionResult VolunteerList(string response = "")
+        {
+            ViewBag.response = response;
+            List<VolunteerPlaces> VoluList;
+            using (DikanDbContext ctx = new DikanDbContext())
+            {
+                VoluList = ctx.VolunteerPlaces.ToList();
+            }
+            return View(VoluList);
+        }
+
+        [HttpGet]
+        public ActionResult CreateEditVol(int? id)
+        {
+            ViewBag.Header = "עדכון מקום התנדבות";
+            VolunteerPlaces TempVol;
+            using (DikanDbContext ctx = new DikanDbContext())
+            {
+                TempVol = ctx.VolunteerPlaces.Where(s => s.Id == id).FirstOrDefault();
+            }
+            if (TempVol != null)
+                return View(TempVol);
+            else
+            {
+                ViewBag.Header = "הוספת מקום התנדבות";
+                return View();
+            }
+
+        }
+
+        [HttpPost]
+        public ActionResult CreateEditVol(VolunteerPlaces ClientVol)
+        {
+            string res = string.Empty;
+            if (ModelState.IsValid)
+            {
+                using (DikanDbContext ctx = new DikanDbContext())
+                {
+                    VolunteerPlaces Temp = ctx.VolunteerPlaces.Where(s => s.Id == ClientVol.Id).FirstOrDefault();
+                    if (Temp == null)
+                    {
+                        res = "מקום התנדבות נוסף בהצלחה";
+                        ctx.VolunteerPlaces.Add(ClientVol);
+                    }
+                    else
+                    {
+                        res = "מקום התנדבות עודכן בהצלחה";
+                        ctx.Entry(Temp).CurrentValues.SetValues(ClientVol);
+                    }
+                    ctx.SaveChanges();
+                }
+            }
+            return RedirectToAction("VolunteerList", new { response = res });
+        }
+
+        [HttpGet]
+        public ActionResult DeleteVol(int VolId)
+        {
+            string res = string.Empty;
+            using (DikanDbContext ctx = new DikanDbContext())
+            {
+                VolunteerPlaces temp = ctx.VolunteerPlaces.Where(s => s.Id == VolId).FirstOrDefault();
+                if (temp != null)
+                {
+                    ctx.VolunteerPlaces.Remove(temp);
+                    ctx.SaveChanges();
+                    res = "מקום התנדבות נמחק בהצלחה";
+                }
+            }
+            return RedirectToAction("VolunteerList", new { response = res });
+        }
+
         #endregion
     }
 }
