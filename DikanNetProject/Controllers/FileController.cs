90a09ec9c534df60c1a4a9aa0980e97b9997cd51
Ron Nahmias <ron.nahmias2@gmail.com>
2019-09-05 17:20:32 +0300

    GetFile Fix

--- DikanNetProject/Controllers/FileController.cs
+++ DikanNetProject/Controllers/FileController.cs
@@ -10,20 +10,14 @@ namespace DikanNetProject.Controllers
     [RequireHttps]
     public class FileController : Controller
     {
+        [HttpGet]
         [Authorize]
-        [HttpPost]
-        public ActionResult GetFile(string pFilePath)
+        public ActionResult GetFile(string pFilePath) // return file acording to file path and user name
         {
-            //return File(Path.Combine(Server.MapPath("~/App_Data/UsersFiles/"), User.Identity.Name, pFilePath), MimeMapping.GetMimeMapping(pFilePath), pFilePath);
-
-            string path = Path.Combine(Server.MapPath("~/App_Data/UsersFiles/"), User.Identity.Name, pFilePath);
-            var stream = new FileStream(path, FileMode.Open, FileAccess.Read);
-
-            string contentType = MimeMapping.GetMimeMapping(path);
-
-            var result = new FileStreamResult(stream, contentType);
-
-            return result;
+            string path = Path.Combine(Server.MapPath("~/App_Data/UsersFiles/"), User.Identity.Name, pFilePath); // path of the file
+            string contentType = MimeMapping.GetMimeMapping(path); // type of file
+            FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+            return File(fs, contentType); // return the file
         }
     }
 }
