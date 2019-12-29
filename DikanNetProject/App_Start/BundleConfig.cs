d9ea48014aae274336f78230fdad5175b344f8ce
Roi Israeli <roi467@gmail.com>
2019-12-29 23:15:42 +0200

    add form slider to new socio

--- DikanNetProject/App_Start/BundleConfig.cs
+++ DikanNetProject/App_Start/BundleConfig.cs
@@ -18,6 +18,10 @@ namespace DikanNetProject
                       "~/Scripts/MyJs/chosen.min.js",
                       "~/Scripts/MyJs/MyJS.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/formSlider").Include(
+                  "~/Scripts/MyJs/jquery-ui.min.js",
+                  "~/Scripts/MyJs/formSlider.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/signature_pad").Include(
                       "~/Scripts/MyJs/signature_pad.umd.js",
                       "~/Scripts/MyJs/signature_app.js"));
@@ -30,6 +34,9 @@ namespace DikanNetProject
                       "~/Content/Css/bootstrap.css",
                       "~/Content/Css/Chosen/chosen.min.css",
                       "~/Content/Css/BaseCSS.css"));
+
+            bundles.Add(new StyleBundle("~/Content/formSlider").Include(
+                      "~/Content/Css/formSlider.css"));
 
             bundles.Add(new StyleBundle("~/Content/datepicker").Include(
                       "~/Content/Css/datepicker.css"));
