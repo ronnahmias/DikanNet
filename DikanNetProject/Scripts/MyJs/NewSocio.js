30b53006a53c5d452391b6943adf2e1f410396fe
Roi Israeli <roi467@gmail.com>
2020-01-03 15:31:09 +0200

    more work on fund ajax

--- DikanNetProject/Scripts/MyJs/NewSocio.js
+++ DikanNetProject/Scripts/MyJs/NewSocio.js
@@ -1,6 +1,8 @@
 ﻿$(document).ready(function () {
 
     console.log("new socio");
+    var fund_list;
+
     $.ajax({
         type: "GET",
         url: '/Student/GetFundings',
@@ -8,25 +10,54 @@
         contentType: "application/json; charset=utf-8",
         success: function (data) {
             console.log(data);
+            fund_list = data;
+            console.log({ fund_list});
         },
         failure: function (errMsg) { alert(errMsg); }
     });
 
+    
+    
     $('#AddFund').click(function () {
         var fund = {
             FinancingInstitution: "בדיקה"
         }
-
+        var btn = $(this);
         $.ajax({
             type: "POST",
             url: '/Student/AddFund',
             dataType: "json",
             data: fund,
+            beforeSend: function () {
+                console.log(btn);
+                btn.attr('disabled', true); // disabel the button
+            },
             success: function (data) {
                 console.log(data);
                 console.log("OK");
             },
-            failure: function (errMsg) { alert(errMsg); }
+            failure: function (errMsg) {
+                alert(errMsg);
+            },
+            complete: function () {
+                btn.removeAttr("disabled"); // enable the button
+            }         
         });
     });
 });
+/*
+function AJAXCALLDATA(pType, pUrl, pData) {
+    $.ajax({
+        type: pType,
+        url: pUrl,
+        dataType: "json",
+        data: pData,
+        success: function (data) {
+            console.log(data);
+            console.log("OK");
+            return data;
+        },
+        failure: function (errMsg) { console.log(errMsg); return false; }
+    });
+}
+*/
